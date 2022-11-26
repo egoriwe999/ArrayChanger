d5e3f87fdbfbe0562a2ee6e78fbe24a2ef98be80
egoriwe999 <80163357+egoriwe999@users.noreply.github.com>
2022-11-27 01:37:16 +0300

    Create f2.h

---
+++ f2.h
@@ -0,0 +1,6 @@
+int* input(int* n); // вввод массива
+void output(int* arr,int n); // вывод массива
+void remove_array(int* arr, int* n,int dx); // удаление элемента в массиве
+void insert(int** arr,int* n, int ix, int dx); // вставка нового элемента в массив
+int sumarray(int **arr, int* n,int **arr2, int* n2); // нахождение максимальной подсуммы , удаление ее из старого массива и перенос в новый массив
+
