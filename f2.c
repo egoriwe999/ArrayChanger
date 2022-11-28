58321d33322f03102837514f86792df46692af4c
egoriwe999 <80163357+egoriwe999@users.noreply.github.com>
2022-11-28 21:04:13 +0300

    Update f2.c

--- f2.c
+++ f2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int* input(int *n){
+int* input(int *n, int *arr){
         int* arr = (int*)malloc((*n)*sizeof(int));
         for(size_t i = 0;i < (*n);i++){
                 printf("a[%d] = ", i);
